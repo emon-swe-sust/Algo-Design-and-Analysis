e441566c346c647a5f93e47f9bed614213a4ed34
Emon(2017831003) <43314141+emon-swe-sust@users.noreply.github.com>
2019-07-14 14:46:37 +0600

    Update Task1.cpp

--- Task1.cpp
+++ Task1.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-    long long redix,i,j,word=0,mod = 100000+3;
+    long long redix,i,j,word=1,mod = 100000+3;
     vector<long long> vec;
     string str;
     cout << "Redix: ";
@@ -18,7 +18,7 @@ int main()
     for(i=0;i<str.size();i++)
     {
         string str1 = "";
-        long long num=0;
+        long long num=0,num1=0,numb,power;
         while(1)
         {
             if(str[i] == ' ' || str[i] == ',' || str[i] == '.' || str[i] == '/' || str[i] == '?')
@@ -28,27 +28,28 @@ int main()
             if(i >= str.size())
                 break;
         }
+        cout << str1 << " - ";
+        num=0;
+        power=1;
         for(j=0;j<str1.size();j++)
         {
-            long long numb,power;
-            numb = (long long)str[j];
-            power = pow(redix,j);
+            numb = (long long)str1[j];
+
+
             num = (num + (numb)*(power))%mod;
+            num1 = num1 + numb*power;
+            power*=redix;
+            power%=mod;
         }
-        vec.push_back(num%mod);
+        vec.push_back(num);
+        //cout << num << " " << num1 << endl;
     }
     sort(vec.begin(),vec.end());
-    for(i=0;i<vec.size();i++)
+    for(i=1;i<vec.size();i++)
     {
         long long num;
         num = vec[i];
-        while(vec[i] == num)
-        {
-            i++;
-            if(i >= vec.size())
-                break;
-        }
-        i--;
+        if(vec[i]!=vec[i-1])
         word++;
     }
 
